86d4945836038521f4c500cc6dcde5cbc7d406ea
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-11-24 20:57:28 +0530

    Add files via upload

---
+++ Mathematical Thinking in Computer Science/N Queens Backtracking.py
@@ -0,0 +1,29 @@
+def can_be_extended_to_solution(perm):
+    i = len(perm) - 1
+    for j in range(i):
+        if i - j == abs(perm[i] - perm[j]):
+            return False
+    return True
+
+def extend(perm, n):
+    if len(perm) == n:
+        print(perm)
+        exit()
+
+    for k in range(n):
+        if k not in perm:
+            perm.append(k)
+
+            if can_be_extended_to_solution(perm):
+                extend(perm, n)
+
+            perm.pop()
+
+extend(perm = [], n = 20)
+'''
+Output
+
+[0, 2, 4, 1, 3, 12, 14, 11, 17, 19, 16, 8, 15, 18, 7, 9, 6, 13, 5, 10]
+
+'''
+
