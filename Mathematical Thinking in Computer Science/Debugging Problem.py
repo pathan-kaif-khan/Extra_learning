b96192f87ee747141998d9a7b9075220b49478a3
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-11-27 18:55:33 +0530

    Fix bug counting logic in debugging simulation
    The code simulates Bob's bug fixing process, but it does not account for the new bugs introduced after fixing each one.

--- Mathematical Thinking in Computer Science/Debugging Problem.py
+++ Mathematical Thinking in Computer Science/Debugging Problem.py
@@ -1,3 +1,21 @@
+'''Problem. Bob is debugging his code. When he starts, he has only one bug. But once he fixes one bug,
+three new bugs appear. Last night Bob fixed 
+15
+15 bugs. How many pending bugs does Bob have now?
+
+Let us model a few first steps of this (not so complicated) process.
+
+Initially, Bob has one pending bug. He fixes it and introduces three new bugs.
+
+Bob has three pending bugs. He fixes one and gets three new bugs.
+
+Bob has five pending bugs. He fixes one of them, but this gives rise to three fresh bugs.
+
+Bob has seven pending bugs
+…
+…dots'''
+
+
 number_of_pending_bugs = 1
 
 for _ in range(15):
